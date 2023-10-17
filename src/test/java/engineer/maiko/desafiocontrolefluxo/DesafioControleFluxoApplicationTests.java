8ce8b53d7183b050846d3379dade7c26bc94a555
Maiko Duarte <mkdu185@gmail.com>
2023-10-17 18:39:11 -0300

    start: Initial commit

---
+++ src/test/java/engineer/maiko/desafiocontrolefluxo/DesafioControleFluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.maiko.desafiocontrolefluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioControleFluxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
